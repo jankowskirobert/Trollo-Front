e93e09d205c44c573a313c7a8c42cac105397a71
jankowskirobert <jankowski.r@hotmail.com>
2017-10-18 22:01:03 +0200

    add bootstrap and jquery

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+require('bootstrap');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    initListeners(); // add listers
+
+});
+
+function initListeners() {
+    console.log('initListeners called');
+
+}
