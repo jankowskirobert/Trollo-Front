d35c07e3032dea7d001569e05aa1a56b57bbaffa
Nerond32 <nerond32@gmail.com>
2017-12-05 23:07:10 +0100

    Login form restyled

--- Login/View.elm
+++ Login/View.elm
@@ -18,22 +18,69 @@ heading : Attribute msg
 heading =
     style
         [ ( " text-align", "center" )
-        , ( "margin-bottom", "30px" )
+        , ( "padding-top", "15px" )
+        , ( "padding-left", "15px" )
+        , ( "padding-right", "15px" )
+        , ( "color", "white" )
         ]
 
 
 loginForm : Html Msg
 loginForm =
-    div [ class "wrapper" ]
+    div [ loginFormStyle ]
         [ Html.form []
             [ h3 [ heading ] [ text "Welcome Back! Please Sign In" ]
             , hr [] []
-            , input [ placeholder ("Enter username "), onInput SetUsername ] []
-            , input [ placeholder ("Enter password "), onInput SetPassword ] []
+            , input [ inputStyle, placeholder ("Enter username "), onInput SetUsername ] []
+            , p [] []
+            , input [ inputStyle, placeholder ("Enter password "), onInput SetPassword ] []
+            , p [] []
             , button
-                [ class "btn btn-success"
+                [ loginButtonStyle
                 , onClick (SignIn)
                 ]
                 [ text "Sign in" ]
+            , button
+                [ loginButtonStyle
+                , onClick (Register) -- ACTION TO BE ADDED
+                ]
+                [ text "Register" ]
             ]
+        ]
+
+
+loginFormStyle : Attribute msg
+loginFormStyle = 
+    style [   ( "margin", "auto" )
+          ,   ( "color", "#646464" )
+          ,   ( "backgroundColor", "#3D88BF" )
+          ,   ( "width", "230px" )
+          ,   ( "height", "250px" )
+          ,   ( "margin-top", "30px" )
+
+          ]
+
+inputStyle : Attribute msg
+inputStyle =
+    style
+        [ ( " text-align", "center" )
+        , ( "margin-left", "30px" )
+        , ( "margin-top", "8px" )
+        ]
+
+loginButtonStyle : Attribute msg
+loginButtonStyle =
+    style
+        [ ( "color", "#646464" )
+        , ( "backgroundColor", "#E3EBEE" )
+        , ( "font-size", "16" )
+        , ( "border", "none" )
+        , ( "overflow", "hidden" )
+        , ( "outline", "none" )
+        , ( "height", "30px" )
+        , ( "margin-left", "32px" )
+        , ( "margin-top", "8px" )
+        , ( "webkit-box-shadow", "0px 2px 2px 0px rgba(211,211,211,1)" )
+        , ( "moz-box-shadow", "0px 2px 2px 0px rgba(211,211,211,1)" )
+        , ( "box-shadow", "0px 2px 2px 0px rgba(211,211,211,1)" )
         ]
